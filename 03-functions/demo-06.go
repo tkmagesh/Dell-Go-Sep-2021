981b5d5dec27edb6fdd9109023ae4e8adb14d720
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-20 17:22:45 +0530

    closures - assignment - solution

--- 03-functions/demo-06.go
+++ 03-functions/demo-06.go
@@ -3,19 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	increment := getIncrementer()
+	increment, decrement := getIncrementer()
 	fmt.Println(increment())
 	fmt.Println(increment())
 	fmt.Println(increment())
 	fmt.Println(increment())
+
+	fmt.Println(decrement()) // => 3
+	fmt.Println(decrement()) // => 2
+	fmt.Println(decrement()) // => 1
+	fmt.Println(decrement()) // => 0
+	fmt.Println(decrement()) // => -1
 
 }
 
-func getIncrementer() func() int { //step 1
+func getIncrementer() (func() int, func() int) { //step 1
 	var counter int = 0       // step 2
 	increment := func() int { //step 3
 		counter++ //step 4
 		return counter
 	}
-	return increment //step 5
+	decrement := func() int {
+		counter--
+		return counter
+	}
+	return increment, decrement //step 5
 }
