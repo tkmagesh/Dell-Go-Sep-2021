ee80f4e605dcd39a99e857a5b8698bae4240886d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-20 16:28:27 +0530

    functions as arguments

---
+++ 03-functions/demo-04.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*
+		fmt.Println("before invocation")
+		fmt.Println(add(100, 200))
+		fmt.Println("after invocation")
+
+		fmt.Println("before invocation")
+		fmt.Println(subtract(100, 200))
+		fmt.Println("after invocation")
+	*/
+	logOperation(add, 100, 200)
+	logOperation(subtract, 100, 200)
+	multiply := func(x, y int) int {
+		return x * y
+	}
+	logOperation(multiply, 100, 200)
+}
+
+func logOperation(oper func(int, int) int, x int, y int) {
+	fmt.Println("before invocation")
+	fmt.Println(oper(x, y))
+	fmt.Println("after invocation")
+}
+
+func add(x, y int) int {
+	result := x + y
+	return result
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
