0a90ecd98feec357aa944b66eed975e001da62cb
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-22 17:43:51 +0530

    io complete

---
+++ 11-io/demo-08.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		log.Println(scanner.Text())
+	}
+}
---
+++ 11-io/demo-11.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Category struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Product struct {
+	Id       int      `json:"id"`
+	Name     string   `json:"name"`
+	Cost     float32  `json:"cost"`
+	Units    int      `json:"units"`
+	Category Category `json:"category"`
+}
+
+/* fmt.Stringer interface implementation */
+func (p Product) String() string {
+	return fmt.Sprintf("Id = %d, Name = %s, Cost = %v, Units = %d, Category = %s", p.Id, p.Name, p.Cost, p.Units, p.Category)
+}
+
+func main() {
+	var p Product
+	file, err := os.Open("product.json")
+	defer file.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	decoder := json.NewDecoder(file)
+	if err = decoder.Decode(&p); err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(p)
+	fmt.Println("Done")
+}
